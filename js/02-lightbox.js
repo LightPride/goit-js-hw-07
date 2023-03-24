57bcce13d81b313132649fd9b7bf5e800dbde4e8
LightPride <vlad.grinevich1@gmail.com>
2023-03-24 17:12:47 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryOfImages = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join("");
+
+galleryOfImages.insertAdjacentHTML("beforeend", markup);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+});
